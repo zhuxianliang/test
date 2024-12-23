1dddd271ca6995d7f03c46e2eeee7d597be71552
v-zhuxianliang <v-zhuxianliang@360.cn>
2024-12-23 17:54:32 +0800

    feat: 完成桑基图

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,19 @@
 import treemap from './components/treemap.vue';
 import treemap1 from './components/treemap2.vue';
 import skany from './components/skany.vue';
+import skany1 from './components/skany2.vue';
 </script>
 
 <template>
   <div class="h-full w-full" style="background-color: #151515">
     <div class="flex justify-center pt-10">
+      <div class="h-[200px] w-[400px] border-2 border-[#333]">
+        <img
+          class="h-full w-full"
+          src="./assets/img/treemap.png"
+          alt=""
+          srcset="" />
+      </div>
       <div class="h-[200px] w-[400px] border-2 border-[#333]">
         <treemap />
         <div class="text-center text-white">
@@ -22,15 +30,24 @@ import skany from './components/skany.vue';
     </div>
     <div class="flex justify-center pt-20">
       <div class="h-[200px] w-[400px] border-2 border-[#333]">
+        <img
+          class="h-full w-full"
+          src="./assets/img/skany.png"
+          alt=""
+          srcset="" />
+      </div>
+      <div class="h-[200px] w-[400px] border-2 border-[#333]">
         <skany />
         <div class="text-center text-white">
-          矩形边框raba，中间链接线无法使用渐变
+          矩形边框raba，中间链接线无法使用渐变 , 无法设置单个矩形宽度
         </div>
       </div>
-      <!-- <div class="w-[400px] h-[200px] border-2 border-[#333]">
-        <treemap1 />
-        <div class=" text-center text-white">背景图,如果里面还有子项，该方案无法使用，内容垂直布局，但不能单个设置样式</div>
-      </div> -->
+      <div class="h-[200px] w-[400px] border-2 border-[#333]">
+        <skany1 />
+        <div class="text-center text-white">
+          中间链接线渐变只能从开始位置到结束位置渐变，但左右两边矩形无法使用渐变，只能（rgb、rgba），无法设置单个矩形宽度
+        </div>
+      </div>
     </div>
   </div>
 </template>
